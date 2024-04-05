b9cae0cb970c94a766a3f60438035f67a5617d36
Nina B <ngbuscemi@gmail.com>
2024-04-05 12:35:38 -0400

    added comments to index routes file

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
+// Imports the express file
 const router = require('express').Router()
+
+// Imports the api routes from api directory
 const apiRoutes = require('./api')
 
+// Mounts api routes so the routes are accessible under /api
 router.use('/api', apiRoutes)
 
+// Fallback route for handling errors
 router.use((req, res) => {
   return res.send('404 Error!')
 })
 
+// Exports the main router for other parts of the app
 module.exports = router
